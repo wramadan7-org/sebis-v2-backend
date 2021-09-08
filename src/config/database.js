7c3b094e6bd300cc8110e065b3cc130591c5d472
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-08 13:49:51 +0700

    Change log error to log info

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ const { host, port, db, user, password } = database;
 const sequelize = new Sequelize(`mariadb://${user}:${password}@${host}:${port}/${db}`);
 
 const connectDb = sequelize.authenticate().then(() => {
-  logger.error('Connection has been established successfully.');
+  logger.info('Connection has been established successfully.');
   return Promise.resolve(true);
 }).catch((error) => {
   logger.error(`Unable to connect to the database: ${error}`);
