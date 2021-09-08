1b9952db9657c1a8dc727827615e5432ee5d2dc9
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-08 11:58:31 +0700

    Add database configuration

--- src/config/config.js
+++ src/config/config.js
@@ -8,6 +8,11 @@ const envVarsSchema = Joi.object().keys({
   NODE_ENV: Joi.string().valid('production', 'development', 'test').required().description('Node app environment'),
   PORT: Joi.number().default(3000).description('Node app listen port'),
   JWT_SECRET: Joi.string().required().description('JWT secret key'),
+  DB_HOST: Joi.string().default('localhost').description('Database host'),
+  DB_PORT: Joi.number().default(3306).description('Database port'),
+  DB_NAME: Joi.string().required().description('Database name'),
+  DB_USER: Joi.string().required().description('Database user'),
+  DB_PASSWORD: Joi.string().required().description('Database password'),
 }).unknown();
 
 const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
@@ -17,7 +22,14 @@ if (error) throw new Error(`Config validation error: ${error.message}`);
 module.exports = {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
+  database: {
+    host: envVars.DB_HOST,
+    port: envVars.DB_PORT,
+    db: envVars.DB_NAME,
+    user: envVars.DB_USER,
+    password: envVars.DB_PASSWORD,
+  },
   jwt: {
     secret: envVars.JWT_SECRET,
   },
-}
+};
