dede7ebb96c8efcb558aa59d36c54c15e7995da1
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-08 21:44:25 +0700

    Import database configuration from sequelize instead

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,28 @@
 const dotenv = require('dotenv');
 const path = require('path');
 const Joi = require('joi');
+const fs = require('fs');
 
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 
+// load database configuration from config.json instead of .env file
+const { NODE_ENV } = process.env;
+const dbConfig = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
+Object.assign(process.env, {
+  DB_DIALECT: dbConfig[NODE_ENV].dialect,
+  DB_HOST: dbConfig[NODE_ENV].host,
+  DB_PORT: dbConfig[NODE_ENV].port,
+  DB_NAME: dbConfig[NODE_ENV].database,
+  DB_USER: dbConfig[NODE_ENV].username,
+  DB_PASSWORD: dbConfig[NODE_ENV].password,
+});
+
+// validate configuration
 const envVarsSchema = Joi.object().keys({
   NODE_ENV: Joi.string().valid('production', 'development', 'test').required().description('Node app environment'),
   PORT: Joi.number().default(3000).description('Node app listen port'),
   JWT_SECRET: Joi.string().required().description('JWT secret key'),
+  DB_DIALECT: Joi.string().default('mysql').description('Database dialect'),
   DB_HOST: Joi.string().default('localhost').description('Database host'),
   DB_PORT: Joi.number().default(3306).description('Database port'),
   DB_NAME: Joi.string().required().description('Database name'),
@@ -18,30 +33,6 @@ const envVarsSchema = Joi.object().keys({
 const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
 
 if (error) throw new Error(`Config validation error: ${error.message}`);
-
-const dbCredentials = {
-  production: {
-    username: `${envVars.DB_USER}`,
-    password: `${envVars.DB_PASSWORD}`,
-    database: `${envVars.DB_NAME}`,
-    host: `${envVars.DB_HOST}`,
-    dialect: "mariadb",
-  },
-  development: {
-    username: `${envVars.DB_USER}`,
-    password: `${envVars.DB_PASSWORD}`,
-    database: `${envVars.DB_NAME}-development`,
-    host: `${envVars.DB_HOST}`,
-    dialect: "mariadb",
-  },
-  test: {
-    username: `${envVars.DB_USER}`,
-    password: `${envVars.DB_PASSWORD}`,
-    database: `${envVars.DB_NAME}-test`,
-    host: `${envVars.DB_HOST}`,
-    dialect: "mariadb",
-  },
-}
 
 module.exports = {
   env: envVars.NODE_ENV,
@@ -56,5 +47,4 @@ module.exports = {
   jwt: {
     secret: envVars.JWT_SECRET,
   },
-  dbCredentials,
 };
