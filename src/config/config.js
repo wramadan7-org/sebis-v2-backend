7b5b20fb3f4c92c7458ee2a103399dc652f221de
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-08 09:32:01 +0700

    Using 2 spaces indent

--- src/config/config.js
+++ src/config/config.js
@@ -5,9 +5,9 @@ const Joi = require('joi');
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const envVarsSchema = Joi.object().keys({
-    NODE_ENV: Joi.string().valid('production', 'development', 'test').required().description('Node app environment'),
-    PORT: Joi.number().default(3000).description('Node app listen port'),
-    JWT_SECRET: Joi.string().required().description('JWT secret key'),
+  NODE_ENV: Joi.string().valid('production', 'development', 'test').required().description('Node app environment'),
+  PORT: Joi.number().default(3000).description('Node app listen port'),
+  JWT_SECRET: Joi.string().required().description('JWT secret key'),
 }).unknown();
 
 const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
@@ -15,9 +15,9 @@ const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' }
 if (error) throw new Error(`Config validation error: ${error.message}`);
 
 module.exports = {
-    env: envVars.NODE_ENV,
-    port: envVars.PORT,
-    jwt: {
-        secret: envVars.JWT_SECRET,
-    },
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+  jwt: {
+    secret: envVars.JWT_SECRET,
+  },
 }
