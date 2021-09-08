47be333c0624cbc5af192500aa5f79504b219ede
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-08 09:42:07 +0700

    Add rateLimiter.js

---
+++ src/middlewares/rateLimiter.js
@@ -0,0 +1,11 @@
+const rateLimit = require('express-rate-limit');
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 20,
+  skipSuccessfulRequests: true,
+});
+
+module.exports = {
+  authLimiter,
+};
