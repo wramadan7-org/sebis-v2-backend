ccee58a14bcf00b0203caeb9effcca01cfb8cb95
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-08 11:57:51 +0700

    Add database.js

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+const logger = require('./logger');
+const { Sequelize } = require('sequelize');
+const { database } = require('./config');
+const { host, port, db, user, password } = database;
+const sequelize = new Sequelize(`mariadb://${user}:${password}@${host}:${port}/${db}`);
+
+const connectDb = sequelize.authenticate().then(() => {
+  logger.error('Connection has been established successfully.');
+  return Promise.resolve(true);
+}).catch((error) => {
+  logger.error(`Unable to connect to the database: ${error}`);
+  return Promise.reject(false);
+});
+
+module.exports = {
+  connectDb,
+  sequelize,
+};
