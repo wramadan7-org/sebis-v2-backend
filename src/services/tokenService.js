81388a5e01de10541dd43423bc2319e29d859454
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-09 15:06:29 +0700

    Add tokenService.js

---
+++ src/services/tokenService.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+const config = require('../config/config');
+const { tokenTypes } = require('../config/tokens');
+
+const generateToken = (userId, expires, type, secret = config.jwt.secret) => {
+  const payload = {
+    sub: userId,
+    iat: moment().unix(),
+    exp: expires.unix(),
+    type,
+  };
+  return jwt.sign(payload, secret);
+};
+
+const generateAuthTokens = async (user) => {
+  const accessTokenExpires = moment().add(config.jwt.accessExpirationMinutes, 'minutes');
+  const accessToken = generateToken(user.id, accessTokenExpires, tokenTypes.ACCESS);
+
+  const refreshTokenExpires = moment().add(config.jwt.refreshExpirationDays, 'days');
+  const refreshToken = generateToken(user.id, refreshTokenExpires, tokenTypes.REFRESH);
+
+  return {
+    access: {
+      token: accessToken,
+      expires: accessTokenExpires.toDate(),
+    },
+    refresh: {
+      token: refreshToken,
+      expires: refreshTokenExpires.toDate(),
+    },
+  };
+};
+
+module.exports = {
+  generateToken,
+  generateAuthTokens,
+};
