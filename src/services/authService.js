71f3b6a5d4bde791568f4ed4f0ec83db760e9b4c
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-09 20:01:01 +0700

    Simplify login logic

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,20 +3,12 @@ const httpStatus = require('http-status');
 const { getUserByEmail } = require('./userService');
 const ApiError = require('../utils/ApiError');
 
-const validatePassword = async (user, password) => {
-  const filterPassword = password === undefined ? '' : password;
-  const loginValid = await bcrypt.compare(filterPassword, user.password);
-  if (!loginValid) throw new ApiError(httpStatus.UNAUTHORIZED, 'Invalid identity combination.');
-};
-
 const loginWithIdentityAndPassword = async (identity, password) => {
   const user = await getUserByEmail(identity);
-  if (user === null) throw new ApiError(httpStatus.UNAUTHORIZED, 'Invalid identity combination.');
-  await validatePassword(user, password);
+  if (user === null || !await bcrypt.compare(password, user.password)) throw new ApiError(httpStatus.UNAUTHORIZED, 'Invalid identity combination.');
   return user;
 };
 
 module.exports = {
-  validatePassword,
   loginWithIdentityAndPassword,
 };
