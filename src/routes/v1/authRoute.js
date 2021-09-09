942fce0e7b54ad45a2f464da8ade42649adaa03b
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-09 20:00:06 +0700

    Add validate middleware on authRoute

--- src/routes/v1/authRoute.js
+++ src/routes/v1/authRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const authController = require('../../controllers/authController');
+const validate = require('../../middlewares/validate');
+const { login, register } = require('../../validations/authValidation');
 
 const router = express.Router();
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/register', validate(register), authController.register);
+router.post('/login', validate(login), authController.login);
 
 module.exports = router;
