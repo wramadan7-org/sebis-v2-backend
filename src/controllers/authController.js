20e556fb12f9018fe34da3e071be49f1c4e37281
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-09 15:02:17 +0700

    Add login handler

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,14 +1,25 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
+const { createUser, getUserByEmail, validatePassword } = require('../services/userService');
+const {generateAuthTokens} = require("../services/tokenService");
+
+const register = catchAsync(async (req, res) => {
+  const userBody = req.body;
+  const user = await createUser(userBody);
+  res.sendWrapped(user, httpStatus.CREATED);
+});
 
 const login = catchAsync(async (req, res) => {
-  const user = {
-    username: 'Hello',
-    password: 'Test123',
-  };
-  res.status(httpStatus.OK).send({ user });
+  const { identity, password } = req.body;
+  const user = await getUserByEmail(identity);
+
+  if (user) await validatePassword(user, password);
+
+  const token = await generateAuthTokens(user);
+  res.sendWrapped(token, httpStatus.OK);
 });
 
 module.exports = {
   login,
+  register,
 };
