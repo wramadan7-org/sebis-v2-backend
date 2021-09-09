751116d9baad9729c07f94d2dde1931ab7b87b85
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-09 19:58:52 +0700

    Add authValidation.js

---
+++ src/validations/authValidation.js
@@ -0,0 +1,22 @@
+const Joi = require('joi');
+
+const register = {
+  body: Joi.object().keys({
+    email: Joi.string().email().max(32).required(),
+    password: Joi.string().min(8).max(32).required(),
+    firstName: Joi.string().max(12).required(),
+    lastName: Joi.string().max(32).required(),
+  }),
+}
+
+const login = {
+  body: Joi.object().keys({
+    identity: Joi.string().max(32).required(),
+    password: Joi.string().min(8).max(32).required(),
+  }),
+};
+
+module.exports = {
+  register,
+  login,
+};
