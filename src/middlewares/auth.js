5f3034b9187b886c62e37a6ebb970c2259eaa1c6
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-10 13:18:36 +0700

    Add auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
+
+const verifyCallback = (req, resolve, reject) => async (err, user, info) => {
+  if (err || info || user === null) {
+    return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate.'));
+  }
+  req.user = user;
+  resolve();
+};
+
+const auth = async (req, res, next) => new Promise((resolve, reject) => {
+  passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject))(req, res, next);
+})
+  .then(() => next())
+  .catch((err) => next(err));
+
+module.exports = auth;
