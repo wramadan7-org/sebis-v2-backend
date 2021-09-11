d9c529914ec9760622a3e72d34aa857ced1e9f13
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-11 20:53:48 +0700

    Add redis getObject

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -7,11 +7,19 @@ const setObject = (key, object) => new Promise((resolve, reject) => {
   });
 });
 
+const getObject = (key) => new Promise((resolve, reject) => {
+  redisClient.hgetall(key, (error, data) => {
+    if (error) reject(error);
+    resolve(data);
+  });
+});
+
 const setExpire = (key, expires = 0) => {
   redisClient.expire(key, expires);
 };
 
 module.exports = {
   setObject,
+  getObject,
   setExpire,
 };
