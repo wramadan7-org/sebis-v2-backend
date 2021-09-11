feaebf08f18ae694e2d8198034023f9cbad6947d
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-11 19:30:05 +0700

    Add logout route

--- src/routes/v1/authRoute.js
+++ src/routes/v1/authRoute.js
@@ -2,10 +2,12 @@ const express = require('express');
 const authController = require('../../controllers/authController');
 const validate = require('../../middlewares/validate');
 const { login, register } = require('../../validations/authValidation');
+const auth = require('../../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/register', validate(register), authController.register);
 router.post('/login', validate(login), authController.login);
+router.post('/logout', auth, authController.logout);
 
 module.exports = router;
