06b15b1f82b460339895bbd478b6fc7a4da8fc4f
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-11 23:31:46 +0700

    Add redisUserLoginKey

--- src/config/redis.js
+++ src/config/redis.js
@@ -4,9 +4,11 @@ const redisClient = redis.createClient();
 
 const redisTokenKey = (userId, type) => `user:jwt:${type}:${userId}`;
 const redisRefreshTokenKey = 'user:jwt:refresh';
+const redisUserLoginKey = 'user:login';
 
 module.exports = {
   redisClient,
   redisTokenKey,
   redisRefreshTokenKey,
+  redisUserLoginKey,
 };
