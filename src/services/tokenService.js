070dd3850839d332425716660c7e5a8f501093bc
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-11 19:26:34 +0700

    Add revokeToken, isTokenActive & some redis helpers

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -2,6 +2,29 @@ const jwt = require('jsonwebtoken');
 const moment = require('moment');
 const config = require('../config/config');
 const { tokenTypes } = require('../config/tokens');
+const { redisClient } = require('../config/redis');
+
+const redisTokenKey = (userId, type) => `user:jwt:${type}:${userId}`;
+
+const signToken = (userId, type, token, ttl = -1) => {
+  redisClient.hmset(`${redisTokenKey(userId, type)}:${token}`, {
+    blacklist: false,
+  });
+  if (ttl > 0) redisClient.expire(`${redisTokenKey(userId, type)}:${token}`, ttl);
+};
+
+const revokeToken = (userId, type, token) => {
+  redisClient.hmset(`${redisTokenKey(userId, type)}:${token}`, {
+    blacklist: true,
+  });
+};
+
+const isTokenActive = (userId, type, token) => new Promise((resolve, reject) => {
+  redisClient.hgetall(`${redisTokenKey(userId, type)}:${token}`, (error, value) => {
+    if (error) reject(error);
+    resolve(value !== null && value.blacklist === 'false');
+  });
+});
 
 const generateToken = (userId, expires, type, secret = config.jwt.secret) => {
   const payload = {
@@ -20,6 +43,9 @@ const generateAuthTokens = async (user) => {
   const refreshTokenExpires = moment().add(config.jwt.refreshExpirationDays, 'days');
   const refreshToken = generateToken(user.id, refreshTokenExpires, tokenTypes.REFRESH);
 
+  // save token to redis
+  signToken(user.id, tokenTypes.ACCESS, accessToken, accessTokenExpires.diff(moment(), 'seconds'));
+
   return {
     access: {
       token: accessToken,
@@ -35,4 +61,6 @@ const generateAuthTokens = async (user) => {
 module.exports = {
   generateToken,
   generateAuthTokens,
+  revokeToken,
+  isTokenActive,
 };
