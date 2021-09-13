ec5fe67711d48c3a2520c59f98d48a6971e2622d
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-13 15:25:30 +0700

    Add fcm utils

---
+++ src/utils/fcm.js
@@ -0,0 +1,54 @@
+const FCM = require('fcm-node');
+const { fcm } = require('../config/config');
+const { teacherKey, studentKey } = fcm;
+const { h } = require('../queues/reminderQueue')
+
+const fcmTeacher = new FCM(teacherKey);
+const fcmStudent = new FCM(studentKey);
+
+/**
+ * Notification sender for teacher
+ * @param {string[]} registrationIds
+ * @param {string} title
+ * @param {string} body
+ * @param {Object} data
+ * @returns {Object}
+ */
+const sendNotificationTeacher = (registrationIds, title, body, data = {}) => {
+  const notificationData = {
+    registration_ids: registrationIds,
+    notification: {
+      title,
+      body,
+    },
+    data,
+  };
+
+  fcmTeacher.send(notificationData, (error, response) => error || response);
+};
+
+/**
+ * Notification sender for student
+ * @param {string[]} registrationIds
+ * @param {string} title
+ * @param {string} body
+ * @param {Object} data
+ * @returns {Object}
+ */
+const sendNotificationStudent = (registrationIds, title, body, data = {}) => {
+  const notificationData = {
+    registration_ids: registrationIds,
+    notification: {
+      title,
+      body,
+    },
+    data,
+  };
+
+  fcmStudent.send(notificationData, (error, response) => error || response);
+};
+
+module.exports = {
+  sendNotificationTeacher,
+  sendNotificationStudent,
+};
