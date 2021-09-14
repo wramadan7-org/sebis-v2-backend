915714cee360ee15b590c9ce6bc852380a035cbd
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-14 23:19:50 +0700

    Refactor authController & add resetPassword handler

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,42 +1,48 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
-const { createUser, getUserById } = require('../services/userService');
-const { loginWithIdentityAndPassword, refreshAuth } = require('../services/authService');
-const { generateAuthTokens, revokeUser } = require('../services/tokenService');
-const { revokeToken } = require('../services/tokenService');
+const userService = require('../services/userService');
+const authService = require('../services/authService');
+const tokenService = require('../services/tokenService');
 const { tokenTypes } = require('../config/tokens');
 
 const register = catchAsync(async (req, res) => {
   const userBody = req.body;
-  const user = await createUser(userBody);
+  const user = await userService.createUser(userBody);
   res.sendWrapped(user, httpStatus.CREATED);
 });
 
 const login = catchAsync(async (req, res) => {
   const { identity, password } = req.body;
-  const user = await loginWithIdentityAndPassword(identity, password);
-  const token = await generateAuthTokens(user);
+  const user = await authService.loginWithIdentityAndPassword(identity, password);
+  const token = await tokenService.generateAuthTokens(user);
   res.sendWrapped(token, httpStatus.OK);
 });
 
 const refreshTokens = catchAsync(async (req, res) => {
   const { refreshToken } = req.body;
-  const tokenData = await refreshAuth(refreshToken);
-  const user = await getUserById(tokenData.userId);
-  const token = await generateAuthTokens(user);
+  const tokenData = await authService.refreshAuth(refreshToken);
+  const user = await userService.getUserById(tokenData.userId);
+  const token = await tokenService.generateAuthTokens(user);
   res.sendWrapped(token, httpStatus.OK);
 });
 
 const logout = catchAsync(async (req, res) => {
   const userId = req.user.id;
   const userToken = req.token;
-  revokeToken(userId, tokenTypes.ACCESS, userToken);
-  revokeUser(userId);
+  tokenService.revokeToken(userId, tokenTypes.ACCESS, userToken);
+  tokenService.revokeUser(userId);
   res.sendWrapped('Logout success.', httpStatus.OK);
 });
 
 const testProtected = catchAsync(async (req, res) => {
   res.sendWrapped('Access granted.', httpStatus.OK);
+});
+
+const resetPassword = catchAsync(async (req, res) => {
+  const userId = req.user.id;
+  const { password } = req.body;
+  await authService.updatePassword(userId, password);
+  res.sendWrapped('Password updated.', httpStatus.OK);
 });
 
 module.exports = {
@@ -45,4 +51,5 @@ module.exports = {
   register,
   logout,
   testProtected,
+  resetPassword,
 };
