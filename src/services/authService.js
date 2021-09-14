bc234267a2b216914797fdb125e96ead6944fe37
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-14 23:47:05 +0700

    Add code documentations

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,18 +5,35 @@ const ApiError = require('../utils/ApiError');
 const redis = require('../utils/redis');
 const { redisRefreshTokenKey } = require('../config/redis');
 
+/**
+ * Login with identity and password
+ * @param {string} identity
+ * @param {string} password
+ * @returns {Promise<User>}
+ */
 const loginWithIdentityAndPassword = async (identity, password) => {
   const user = await userService.getUserByEmail(identity);
   if (!user || !await bcrypt.compare(password, user.password)) throw new ApiError(httpStatus.UNAUTHORIZED, 'Invalid identity combination.');
   return user;
 };
 
+/**
+ * Refresh token auth
+ * @param {string} refreshToken
+ * @returns {Promise<Object>}
+ */
 const refreshAuth = async (refreshToken) => {
   const tokenData = await redis.getObject(`${redisRefreshTokenKey}:${refreshToken}`);
   if (!tokenData || tokenData.blacklist !== 'false') throw new ApiError(httpStatus.UNAUTHORIZED, 'Refresh token not valid.');
   return tokenData;
 };
 
+/**
+ *
+ * @param userId
+ * @param newPassword
+ * @returns {Promise<User | ApiError>}
+ */
 const updatePassword = async (userId, newPassword) => {
   const salt = await bcrypt.genSaltSync(10);
   const password = bcrypt.hashSync(newPassword, salt);
