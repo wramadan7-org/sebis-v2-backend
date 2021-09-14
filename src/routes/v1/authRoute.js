30f76a8ca838a9d58089373cfc3b63a3d76777fa
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-14 23:25:18 +0700

    Refactor authRoute & add reset-password route

--- src/routes/v1/authRoute.js
+++ src/routes/v1/authRoute.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const authController = require('../../controllers/authController');
 const validate = require('../../middlewares/validate');
-const { login, register, refresh } = require('../../validations/authValidation');
+const authValidation = require('../../validations/authValidation');
 const auth = require('../../middlewares/auth');
 
 const router = express.Router();
 
-router.post('/register', validate(register), authController.register);
-router.post('/login', validate(login), authController.login);
+router.post('/register', validate(authValidation.register), authController.register);
+router.post('/login', validate(authValidation.login), authController.login);
 router.post('/logout', auth, authController.logout);
-router.post('/refresh', validate(refresh), authController.refreshTokens);
+router.post('/refresh', validate(authValidation.refresh), authController.refreshTokens);
 router.get('/protected', auth, authController.testProtected);
+router.post('/reset-password', auth, validate(authValidation.resetPassword), authController.resetPassword);
 
 module.exports = router;
