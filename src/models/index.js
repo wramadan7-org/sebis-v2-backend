3a2be18689658880146159f5c4ac98e1f8e2cc80
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-15 10:18:03 +0700

    Add relationship between User & Role

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,12 @@
 const { User } = require('./User');
 const { School } = require('./School');
+const { Role } = require("./Role");
 
 const setupSequelizeAssociations = () => {
   User.belongsTo(School);
   School.hasMany(User);
+  User.belongsTo(Role);
+  Role.hasMany(User);
 };
 
 module.exports = setupSequelizeAssociations;
