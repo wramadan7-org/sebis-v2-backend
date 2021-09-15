53b056d8a0b90f3c5d733644da5a9f7366b10d14
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-15 09:43:04 +0700

    Add user roles migration

---
+++ src/migrations/20210915022401-add-user-roles.js
@@ -0,0 +1,47 @@
+const {DataTypes} = require("sequelize");
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('roles', {
+      id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false,
+      },
+      roleName: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+    await queryInterface.addColumn(
+      'users',
+      'roleId',
+      {
+        type: Sequelize.STRING,
+        references: {
+          model: 'roles',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('roles');
+    await queryInterface.removeColumn(
+      'users',
+      'roleId',
+    );
+  }
+};
