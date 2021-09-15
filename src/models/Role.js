048807aed6924b0c217b88a7f7c00b3ad449a662
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-15 09:46:14 +0700

    Add Role model

---
+++ src/models/Role.js
@@ -0,0 +1,20 @@
+const { DataTypes, Sequelize } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const Role = sequelize.define('user', {
+  id: {
+    type: DataTypes.UUIDV4,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false,
+  },
+  roleName: {
+    type: DataTypes.ENUM('teacher', 'student'),
+    unique: true,
+    allowNull: false,
+  },
+});
+
+module.exports = {
+  Role,
+};
