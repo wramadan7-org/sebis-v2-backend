fc8f8bcfa28bc4b25593099f5a02d572a7a408e9
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-16 14:23:24 +0700

    Add role seeders

---
+++ src/seeders/20210916070600-add-roles.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('roles', [
+      {
+        id: 'a0a76676-e446-49d2-ab7a-ae622783d7b8',
+        roleName: 'student',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: '437e0221-eb3d-477f-a3b3-799256fbcab6',
+        roleName: 'teacher',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('roles', null);
+  }
+};
