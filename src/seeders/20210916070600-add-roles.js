8df5d23027010cf2d5b6847bc860cbcde9f83898
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-16 16:43:10 +0700

    Eslint formatter

--- src/seeders/20210916070600-add-roles.js
+++ src/seeders/20210916070600-add-roles.js
@@ -1,24 +1,18 @@
-'use strict';
-
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('roles', [
-      {
-        id: 'a0a76676-e446-49d2-ab7a-ae622783d7b8',
-        roleName: 'student',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        id: '437e0221-eb3d-477f-a3b3-799256fbcab6',
-        roleName: 'teacher',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ])
-  },
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('roles', [
+    {
+      id: 'a0a76676-e446-49d2-ab7a-ae622783d7b8',
+      roleName: 'student',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: '437e0221-eb3d-477f-a3b3-799256fbcab6',
+      roleName: 'teacher',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ]),
 
-  down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('roles', null);
-  }
+  down: async (queryInterface, Sequelize) => queryInterface.bulkDelete('roles', null),
 };
