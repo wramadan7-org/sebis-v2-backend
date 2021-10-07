a4a92aa633b54896ac2299e0531941930ee8448c
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-10-07 16:58:35 +0700

    Add cart migration

---
+++ src/migrations/20211007084657-create-cart.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.createTable('carts', {
+    id: {
+      type: Sequelize.STRING,
+      primaryKey: true,
+      allowNull: false,
+    },
+    userId: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    deletedAt: {
+      type: Sequelize.DATE,
+    },
+  }),
+
+  down: async (queryInterface, Sequelize) => queryInterface.dropTable('carts'),
+};
