5b5360ed8a4866ded7f0a43b9615b652acf31359
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-10-07 16:59:56 +0700

    Add association user and cart

--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,18 @@
 const { User } = require('./User');
 const { School } = require('./School');
 const { Role } = require('./Role');
+const { Cart } = require('./Cart');
 
 const setupSequelizeAssociations = () => {
   User.belongsTo(Role);
   User.belongsTo(School);
+  User.hasOne(Cart);
 
   School.hasMany(User);
 
   Role.hasMany(User);
+
+  Cart.belongsTo(User);
 };
 
 module.exports = setupSequelizeAssociations;
