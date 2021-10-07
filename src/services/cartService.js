b1e42a33e9d464670d2fc46c10037ef292fa2a4a
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-10-07 16:58:57 +0700

    Add cartService

---
+++ src/services/cartService.js
@@ -0,0 +1,37 @@
+const httpStatus = require('http-status');
+const { Cart } = require('../models/Cart');
+const ApiError = require('../utils/ApiError');
+const userService = require('./userService');
+
+/**
+ * Get cart by user ID
+ * @param {String} userId
+ * @return {Promise<Cart | ApiError>}
+ */
+const getCartByUserId = async (userId) => {
+  const cart = await Cart.findOne({
+    userId,
+  });
+  if (!cart) throw new ApiError(httpStatus.NOT_FOUND, 'Cart not found.');
+  return cart;
+};
+
+/**
+ * Create cart
+ * @param {String} userId
+ * @return {Promise<Cart | ApiError>}
+ */
+const createCart = async (userId) => {
+  await userService.getUserById(userId);
+
+  return Cart.findOrCreate({
+    where: {
+      userId,
+    },
+  });
+};
+
+module.exports = {
+  getCartByUserId,
+  createCart,
+};
