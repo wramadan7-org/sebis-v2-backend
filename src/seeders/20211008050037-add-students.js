32e218f7f9353eddcf39dd00c4e0b4dfdffbbf93
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-10-09 10:17:50 +0700

    Add student seeders

---
+++ src/seeders/20211008050037-add-students.js
@@ -0,0 +1,18 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('users', [
+    {
+      id: 'bae4d026-cd7f-4b5d-8044-87ebb116116f',
+      email: 'student@sebisedu.co.id',
+      password: '$2b$10$WXit0D6QlUKJZkwwCT7.qu1u3g.u6ivDMp7Fs0qZ3tqYJh/dZXfZO', // 12345678
+      firstName: 'Ilham',
+      lastName: 'Student',
+      roleId: 'a0a76676-e446-49d2-ab7a-ae622783d7b8',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ]),
+
+  down: async (queryInterface, Sequelize) => queryInterface.bulkDelete('users', {
+    roleId: 'a0a76676-e446-49d2-ab7a-ae622783d7b8',
+  }),
+};
