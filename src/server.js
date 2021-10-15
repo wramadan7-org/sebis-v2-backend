c7cc4cb84bbe5c99ef3ea19ca3f791001efd2574
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-10-15 20:52:05 +0700

    Optimize server using clusters

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+const cluster = require('cluster');
+const numCpus = require('os').cpus().length;
 const app = require('./app');
 const config = require('./config/config');
 const { connectDb } = require('./config/database');
@@ -18,21 +20,43 @@ const exitHandler = () => {
   }
 };
 
-connectDb
-  .then(() => {
-    setupSequelizeAssociations().then(() => {
-      initializeQueue();
-      server = app.listen(config.port, () => {
-        logger.info(`Listening to port ${config.port}`);
-      });
-    });
-  })
-  .catch(() => exitHandler());
-
 const unexpectedErrorHandler = (error) => {
   logger.error(error);
   exitHandler();
 };
+
+const initializeServer = () => {
+  connectDb
+    .then(() => {
+      setupSequelizeAssociations()
+        .then(() => {
+          initializeQueue();
+          if (cluster.isPrimary) {
+            logger.info(`Primary ${process.pid} is running`);
+
+            // Fork workers.
+            for (let i = 0; i < numCpus; i += 1) {
+              cluster.fork();
+            }
+
+            cluster.on('exit', (worker) => {
+              logger.info(`Worker ${worker.process.pid} died`);
+              logger.info(`Remaining workers: ${Object.keys(cluster.workers).length}`);
+              logger.info('Starting new worker');
+              cluster.fork();
+              logger.info(`Current workers: ${Object.keys(cluster.workers).length}`);
+            });
+          } else {
+            server = app.listen(config.port, () => {
+              logger.info(`Listening to port ${config.port}`);
+            });
+          }
+        });
+    })
+    .catch(() => exitHandler());
+};
+
+initializeServer();
 
 process.on('uncaughtException', unexpectedErrorHandler);
 process.on('unhandledRejection', unexpectedErrorHandler);
