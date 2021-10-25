55b4b326250d2db929e0c4efcb4cfa88c4c06d8a
wramadan7-org <wramadan1203@gmail.com>
2021-10-25 13:46:21 +0700

    validation to update userdetail

--- src/validations/userDetailValidation.js
+++ src/validations/userDetailValidation.js
@@ -16,6 +16,23 @@ const createUserDetail = {
   }),
 };
 
+const updateUserDetail = {
+  body: Joi.object().keys({
+    birthPlace: Joi.string().uppercase().max(32),
+    birthDate: Joi.date(),
+    religion: Joi.string().uppercase().max(12),
+    idCardType: Joi.string().uppercase().max(12),
+    idCardNumber: Joi.string().max(32),
+    mailingAddress: Joi.string().max(32),
+    city: Joi.string().uppercase().max(32),
+    region: Joi.string().uppercase().max(32),
+    postalCode: Joi.string().max(8),
+    country: Joi.string().uppercase().max(32),
+    aboutMe: Joi.string().max(32),
+  }),
+};
+
 module.exports = {
   createUserDetail,
+  updateUserDetail,
 };
