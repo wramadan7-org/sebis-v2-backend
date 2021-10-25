eab2afd3399c3689bd050bd2f70aba2bb8620b1d
wramadan7-org <wramadan1203@gmail.com>
2021-10-25 10:26:58 +0700

    set region uppercase and delete userId in validation

--- src/validations/userDetailValidation.js
+++ src/validations/userDetailValidation.js
@@ -2,7 +2,6 @@ const Joi = require('joi');
 
 const createUserDetail = {
   body: Joi.object().keys({
-    userId: Joi.string().max(64).required(),
     birthPlace: Joi.string().uppercase().max(32).required(),
     birthDate: Joi.date().required(),
     religion: Joi.string().uppercase().max(12).required(),
@@ -10,7 +9,7 @@ const createUserDetail = {
     idCardNumber: Joi.string().max(32).required(),
     mailingAddress: Joi.string().max(32).required(),
     city: Joi.string().uppercase().max(32).required(),
-    region: Joi.string().max(32).required(),
+    region: Joi.string().uppercase().max(32).required(),
     postalCode: Joi.string().max(8).required(),
     country: Joi.string().uppercase().max(32).required(),
     aboutMe: Joi.string().max(32).required(),
