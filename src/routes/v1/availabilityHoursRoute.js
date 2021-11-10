30eb57f681f9274c4b2869c592438f344e9317e4
wramadan7-org <wramadan1203@gmail.com>
2021-11-10 16:56:32 +0700

    routing availability hours

---
+++ src/routes/v1/availabilityHoursRoute.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+
+const availabilityHoursController = require('../../controllers/availabilityHoursController');
+const validate = require('../../middlewares/validate');
+const auth = require('../../middlewares/auth');
+const availabilityHoursValidation = require('../../validations/availabilityHoursValidation');
+
+const router = Router();
+
+router.post('/', auth, validate(availabilityHoursValidation.createScheduleTime), availabilityHoursController.createTutorScheduleTime);
+router.get('/', auth, availabilityHoursController.getTutorScheduleTimeAll);
+router.get('/:availabilityHoursId', auth, validate(availabilityHoursValidation.getScheduleTimeById), availabilityHoursController.getTutorScheduleTimeById);
+
+module.exports = router;
