9ac0d5a223e348229d86fd3c3e8c977c232bc238
wramadan7-org <wramadan1203@gmail.com>
2021-11-16 09:40:14 +0700

    add referral history controller

--- src/controllers/referralHistoryController.js
+++ src/controllers/referralHistoryController.js
@@ -5,5 +5,20 @@ const ApiError = require('../utils/ApiError');
 
 const addReferralHistory = catchAsync(async (req, res) => {
   const userId = req.user.id;
-  const referralBody = req.body;
+  const {
+    referredBy,
+    referralCode,
+  } = req.body;
+
+  const referralHistory = await referralHistoryService.createReferralHistory(
+    userId,
+    referredBy,
+    referralCode,
+  );
+
+  return referralHistory;
 });
+
+module.exports = {
+  addReferralHistory,
+};
