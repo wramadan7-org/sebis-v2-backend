234c3ee42f31f225698248354e407b14a8258aac
wramadan7-org <wramadan1203@gmail.com>
2021-11-16 12:03:44 +0700

    route update referral history

--- src/routes/v1/referralHistoryRoute.js
+++ src/routes/v1/referralHistoryRoute.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
 const referralHistoryController = require('../../controllers/referralHistoryController');
 const auth = require('../../middlewares/auth');
+const validation = require('../../middlewares/validate');
+const referralHistoryValidation = require('../../validations/referralHistoryValidation');
 
 const router = Router();
 
 router.get('/', auth, referralHistoryController.getReferralHistories);
-router.get('/:referralHistoryId', auth, referralHistoryController.getReferralHistoryById);
+router.get('/:referralHistoryId', auth, validation(referralHistoryValidation.getReferralHistoryById), referralHistoryController.getReferralHistoryById);
+router.patch('/:referralHistoryId', auth, validation(referralHistoryValidation.updateReferralHistory), referralHistoryController.updateReferralHistory);
 
 module.exports = router;
