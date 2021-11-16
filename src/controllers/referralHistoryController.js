3d42b8f65057c62f8bf980c0bb95efbcf34bae3c
wramadan7-org <wramadan1203@gmail.com>
2021-11-16 09:45:46 +0700

    get referral history by id controller

--- src/controllers/referralHistoryController.js
+++ src/controllers/referralHistoryController.js
@@ -27,7 +27,22 @@ const getReferralHistories = catchAsync(async (req, res) => {
   return referralHistories;
 });
 
+const getReferralHistoryById = catchAsync(async (req, res) => {
+  const userId = req.user.id;
+  const { referralHistoryId } = req.params;
+
+  const referralHistory = await referralHistoryService.getReferralHistoryById(
+    referralHistoryId,
+    userId,
+  );
+
+  if (!referralHistory) throw new ApiError(httpStatus.NOT_FOUND, 'Referral not found.');
+
+  return referralHistory;
+});
+
 module.exports = {
   addReferralHistory,
   getReferralHistories,
+  getReferralHistoryById,
 };
