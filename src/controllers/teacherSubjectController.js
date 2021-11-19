04720d43731d35476cc618079deabdf9a378d43a
wramadan7-org <wramadan1203@gmail.com>
2021-11-19 16:31:18 +0700

    controller create teacher subject

---
+++ src/controllers/teacherSubjectController.js
@@ -0,0 +1,19 @@
+const httpStatus = require('http-status');
+const catchAsync = require('../utils/catchAsync');
+const teacherSubjectService = require('../services/teacherSubjectService');
+
+const createTeacherSubject = catchAsync(async (req, res) => {
+  const teacherId = req.user.id;
+  const teacherSubjectBody = req.body;
+
+  const teacherSubject = await teacherSubjectService.createTeacherSubject(
+    teacherId,
+    teacherSubjectBody,
+  );
+
+  res.sendWrapped(teacherSubject, httpStatus.CREATED);
+});
+
+module.exports = {
+  createTeacherSubject,
+};
