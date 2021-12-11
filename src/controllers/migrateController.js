0b095b327ef407828cae6b01cda6c72d9e150b24
wramadan7-org <wramadan1203@gmail.com>
2021-12-11 09:37:22 +0700

    migrateController function add user

--- src/controllers/migrateController.js
+++ src/controllers/migrateController.js
@@ -6,15 +6,30 @@ const fs = require('fs');
 const listUser = catchAsync(async (req, res) => {
     const user = await migrateService.listUser();
 
-    if (user.length <= 0) {
-        return res.sendWraped('Data empty', httpStatus.NOT_FOUND);
-    };
+    // if (user.length <= 0) {
+    //     return res.sendWraped('Data empty', httpStatus.NOT_FOUND);
+    // };
 
-    fs.writeFileSync('./public/files/userJson.json', JSON.stringify(user));
+    // fs.writeFileSync('./public/files/userJson.json', JSON.stringify(user));
 
-    res.sendWrapped(user, httpStatus.CREATED);
-})
+    res.sendWrapped(user, httpStatus.OK);
+});
+
+const addUser = catchAsync(async (req, res) => {
+    const user = await migrateService.addUser();
+    fs.writeFileSync('./public/files/user.csv', JSON.stringify(user));
+
+    res.sendWrapped(user, httpStatus.CREATED)
+});
+
+const addUserDetail = catchAsync(async (req, res) => {
+    const userDetail = await migrateService.addUserDetail();
+
+    res.sendWrapped(userDetail, httpStatus.CREATED);
+});
 
 module.exports = {
     listUser,
+    addUser,
+    addUserDetail
 };
