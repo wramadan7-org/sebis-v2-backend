5d57daf33ad21225680de0135b04798a602fac83
wramadan7-org <wramadan1203@gmail.com>
2021-12-18 12:07:22 +0700

    fix format in convertUpnormalDate

--- src/utils/convertUpnormalDate.js
+++ src/utils/convertUpnormalDate.js
@@ -4,19 +4,22 @@ const convertDate = async (data) => {
     const defaultDate = moment('01-01-2000').format('DD-MM-YYYY')
     const spliting = data.split('/');
 
-    if (spliting.length == 2) {
-        if (spliting[0].length == 2 && spliting[1].length == 4 && spliting[0].match(/\d+/g) && spliting[1].match(/\d+/g)) {
-            const newDate = `${spliting[0]}-01-${spliting[1]}`;
-            const momentDate = moment(new Date(newDate)).isValid() === true ? moment(new Date(newDate)).format('DD-MM-YYYY') : defaultDate;
-
-            return momentDate;
+    if (data && data !== null) {
+        if (spliting.length == 2) {
+            if (spliting[0].length == 2 && spliting[1].length == 4 && spliting[0].match(/\d+/g) && spliting[1].match(/\d+/g) && !spliting[0] == '00') {
+                const newDate = `01-${spliting[0]}-${spliting[1]}`;
+                const momentDate = moment(newDate, 'DD-MM-YYYY').isValid() === true ? moment(newDate, 'DD-MM-YYYY').format('DD-MM-YYYY') : defaultDate;
+    
+                return momentDate;
+            } else {
+                return defaultDate;
+            }
         } else {
             return defaultDate;
-        }
-    } else {
-        return defaultDate;
+        };
     };
 
+    return defaultDate;
 };
 
 module.exports = {
