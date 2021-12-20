519cc630db988737c3ee1a6a8c176b490b5a0c5f
wramadan7-org <wramadan1203@gmail.com>
2021-12-20 13:48:07 +0700

    add device model

---
+++ src/models/Device.js
@@ -0,0 +1,24 @@
+const { sequelize, DataTypes, SequelizeInstance } = require('../config/database');
+
+const Device = sequelize.define('device', {
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        defaultValue: SequelizeInstance.UUIDV4,
+        allowNull: false,
+    },
+    userId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    registeredToken: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    paranoid: true,
+});
+
+module.exports = {
+    Device,
+};
