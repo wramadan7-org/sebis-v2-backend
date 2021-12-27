76fcd90a31f251ad814ceb6e411575d21bce8307
wramadan7-org <wramadan1203@gmail.com>
2021-12-27 10:33:45 +0700

    comment all studentController

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -1,13 +1,15 @@
-const httpStatus = require('http-status');
-const catchAsync = require('../utils/catchAsync');
-const cartService = require('../services/cartService');
+// const httpStatus = require('http-status');
+// const catchAsync = require('../utils/catchAsync');
+// const cartService = require('../services/cartService');
 
-const viewCart = catchAsync(async (req, res) => {
-  const studentId = req.user.id;
-  const cart = await cartService.findOrCreateCart(studentId);
-  res.sendWrapped(cart[0], httpStatus.OK);
-});
+// const viewCart = catchAsync(async (req, res) => {
+//   const studentId = req.user.id;
+//   const cart = await cartService.findOrCreateCart(studentId, {
+//     include: 'cartItems',
+//   });
+//   res.sendWrapped(cart[0], httpStatus.OK);
+// });
 
-module.exports = {
-  viewCart,
-};
+// module.exports = {
+//   viewCart,
+// };
