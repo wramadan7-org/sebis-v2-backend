0a95f1951a1ec21028349aa7049a5eabcf024e9d
wramadan7-org <wramadan1203@gmail.com>
2021-12-27 10:34:14 +0700

    comment all studentRoute

--- src/routes/v1/studentRoute.js
+++ src/routes/v1/studentRoute.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const auth = require('../../middlewares/auth');
-const { student } = require('../../middlewares/roleValidation');
-const studentController = require('../../controllers/studentController');
-const cartController = require('../../controllers/cartController');
+// const express = require('express');
+// const auth = require('../../middlewares/auth');
+// const { student } = require('../../middlewares/roleValidation');
+// const studentController = require('../../controllers/studentController');
+// const cartController = require('../../controllers/cartController');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get('/cart', auth, student, studentController.viewCart);
-router.post('/cart', auth, student, cartController.addCart);
+// router.get('/cart', auth, student, cartController.viewCart);
+// router.post('/cart', auth, student, cartController.addCart);
 
-module.exports = router;
+// module.exports = router;
