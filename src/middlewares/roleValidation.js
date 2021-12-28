979f70295cbff26be20f361b52de9e0255b26809
wramadan7-org <wramadan1203@gmail.com>
2021-12-28 11:12:06 +0700

    all role validation

--- src/middlewares/roleValidation.js
+++ src/middlewares/roleValidation.js
@@ -14,7 +14,48 @@ const student = async (req, res, next) => {
   next();
 };
 
+const verifikator = async (req, res, next) => {
+  const { role } = req.user;
+  if (role !== roleTypes.VERIFIKATOR) next(new ApiError(httpStatus.FORBIDDEN, 'Access denied'));
+};
+
+const finance = async (req, res, next) => {
+  const { role } = req.user;
+  if (role !== roleTypes.FINANCE) next(new ApiError(httpStatus.FORBIDDEN, 'Access denied'));
+  next();
+};
+
+const admin = async (req, res, next) => {
+  const { role } = req.user;
+  if (role !== roleTypes.ADMIN) next(new ApiError(httpStatus.FORBIDDEN, 'Access denied'));
+  next();
+};
+
+const administrator = async (req, res, next) => {
+  const { role } = req.user;
+  if (role !== roleTypes.ADMINISTRATOR) next(new ApiError(httpStatus.FORBIDDEN, 'Access denied'));
+  next();
+};
+
+const pteacher = async (req, res, next) => {
+  const { role } = req.user;
+  if (role !== roleTypes.PTEACHER) next(new ApiError(httpStatus.FORBIDDEN, 'Access denied'));
+  next();
+};
+
+const rteacher = async (req, res, next) => {
+  const { role } = req.user;
+  if (role !== roleTypes.RTEACHER) next(new ApiError(httpStatus.FORBIDDEN, 'Access denied'));
+  next();
+};
+
 module.exports = {
   teacher,
   student,
+  verifikator,
+  finance,
+  admin,
+  administrator,
+  pteacher,
+  rteacher,
 };
