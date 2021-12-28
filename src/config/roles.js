02c7100151f36c11119035334cae9b2ac4136469
wramadan7-org <wramadan1203@gmail.com>
2021-12-28 11:08:56 +0700

    all role types

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,6 +1,13 @@
 const roleTypes = {
   TEACHER: 'teacher',
   STUDENT: 'student',
+  VERIFIKATOR: 'verifikator',
+  FINANCE: 'finance',
+  PTEACHER: 'pteacher',
+  RTEACHER: 'rteacher',
+  ADMIN: 'admin',
+  ADMINISTRATOR: 'administrator',
+  PUBLIC: 'public',
 };
 
 module.exports = {
