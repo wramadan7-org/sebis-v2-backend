57c65c1f099211d8ff6a134a278a804e8f075ba6
wramadan7-org <wramadan1203@gmail.com>
2022-01-05 11:19:45 +0700

    controller migrate add availability hours

--- src/controllers/migrateController.js
+++ src/controllers/migrateController.js
@@ -4,76 +4,83 @@ const catchAsync = require('../utils/catchAsync');
 const migrateService = require('../services/migrationService');
 
 const listUser = catchAsync(async (req, res) => {
-    const user = await migrateService.listUser();
+  const user = await migrateService.listUser();
 
-    // if (user.length <= 0) {
-    //     return res.sendWraped('Data empty', httpStatus.NOT_FOUND);
-    // };
+  // if (user.length <= 0) {
+  //     return res.sendWraped('Data empty', httpStatus.NOT_FOUND);
+  // };
 
-    // fs.writeFileSync('./public/files/userJson.json', JSON.stringify(user));
+  // fs.writeFileSync('./public/files/userJson.json', JSON.stringify(user));
 
-    res.sendWrapped(user, httpStatus.OK);
+  res.sendWrapped(user, httpStatus.OK);
 });
 
 const addUser = catchAsync(async (req, res) => {
-    const user = await migrateService.addUser();
-    // fs.writeFileSync('./public/files/userFix.csv', JSON.stringify(user));
+  const user = await migrateService.addUser();
+  // fs.writeFileSync('./public/files/userFix.csv', JSON.stringify(user));
 
-    res.sendWrapped(user, httpStatus.CREATED);
+  res.sendWrapped(user, httpStatus.CREATED);
 });
 
 const addUserDetail = catchAsync(async (req, res) => {
-    const userDetail = await migrateService.addUserDetail();
+  const userDetail = await migrateService.addUserDetail();
 
-    // fs.writeFileSync('./public/files/userDetailFix.json', JSON.stringify(userDetail));
+  // fs.writeFileSync('./public/files/userDetailFix.json', JSON.stringify(userDetail));
 
-    res.sendWrapped(userDetail, httpStatus.CREATED);
+  res.sendWrapped(userDetail, httpStatus.CREATED);
 });
 
 const addTeachingExperiences = catchAsync(async (req, res) => {
-    const teachingExperience = await migrateService.addTeachingExperience();
+  const teachingExperience = await migrateService.addTeachingExperience();
 
-    res.sendWrapped(teachingExperience, httpStatus.CREATED);
+  res.sendWrapped(teachingExperience, httpStatus.CREATED);
 });
 
 const addEducationBackground = catchAsync(async (req, res) => {
-    const educationBakground = await migrateService.addEducationBackground();
+  const educationBakground = await migrateService.addEducationBackground();
 
-    res.sendWrapped(educationBakground, httpStatus.CREATED);
+  res.sendWrapped(educationBakground, httpStatus.CREATED);
 });
 
 const addBank = catchAsync(async (req, res) => {
-    const bank = await migrateService.addBank();
+  const bank = await migrateService.addBank();
 
-    res.sendWrapped(bank, httpStatus.CREATED);
+  res.sendWrapped(bank, httpStatus.CREATED);
 });
 
 const addDevice = catchAsync(async (req, res) => {
-    const devices = await migrateService.addDevice();
+  const devices = await migrateService.addDevice();
 
-    res.sendWrapped(devices, httpStatus.CREATED);
+  res.sendWrapped(devices, httpStatus.CREATED);
 });
 
 const addSubject = catchAsync(async (req, res) => {
-    const subject = await migrateService.addSubject();
+  const subject = await migrateService.addSubject();
 
-    res.sendWrapped(subject, httpStatus.CREATED);
+  res.sendWrapped(subject, httpStatus.CREATED);
 });
 
 const addGradeGroup = catchAsync(async (req, res) => {
-    const grade = await migrateService.addGradeGroup();
+  const grade = await migrateService.addGradeGroup();
 
-    res.sendWrapped(grade, httpStatus.CREATED);
+  res.sendWrapped(grade, httpStatus.CREATED);
+});
+
+const addAvailabilityHours = catchAsync(async (req, res) => {
+  const availabilityHours = await migrateService.addAvailabilityHours();
+
+  res.sendWrapped(availabilityHours, httpStatus.CREATED);
 });
 
 module.exports = {
-    listUser,
-    addUser,
-    addUserDetail,
-    addTeachingExperiences,
-    addEducationBackground,
-    addBank,
-    addDevice,
-    addSubject,
-    addGradeGroup,
+  listUser,
+  addUser,
+  addUserDetail,
+  addTeachingExperiences,
+  addEducationBackground,
+  addBank,
+  addDevice,
+  addSubject,
+  addGradeGroup,
+  addAvailabilityHours,
 };
