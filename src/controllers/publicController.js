d3444d343ce23f9be5ab3ccd529341b69c219a33
wramadan7-org <wramadan1203@gmail.com>
2022-01-10 11:28:36 +0700

    publicController use paginate in function home and time

--- src/controllers/publicController.js
+++ src/controllers/publicController.js
@@ -6,16 +6,30 @@ const catchAsync = require('../utils/catchAsync');
 const publicService = require('../services/publicService');
 
 const publicHome = catchAsync(async (req, res) => {
-  const home = await publicService.homePublic();
+  let { page, limit } = req.query;
+
+  if (limit) {
+    limit = parseInt(limit);
+  } else {
+    limit = 10;
+  }
+
+  if (page) {
+    page = parseInt(page);
+  } else {
+    page = 1;
+  }
+
+  const home = await publicService.homePublic(page, limit);
 
   if (!home) throw new ApiError(httpStatus.NOT_FOUND, 'Empty data');
 
-  res.sendWrapped(home, httpStatus.OK);
+  res.sendWrapped('', httpStatus.OK, home);
 });
 
 const availabilityHours = catchAsync(async (req, res) => {
   const { teacherId } = req.params;
-  const { month } = req.query;
+  let { month, page, limit } = req.query;
 
   let theMonth;
 
@@ -25,13 +39,25 @@ const availabilityHours = catchAsync(async (req, res) => {
     theMonth = month;
   }
 
+  if (page) {
+    page = parseInt(page);
+  } else {
+    page = 1;
+  }
+
+  if (limit) {
+    limit = parseInt(limit);
+  } else {
+    limit = 10;
+  }
+
   const year = moment().format('YYYY');
 
-  const availabilityHoursTutor = await publicService.timeAvailabilityPublic(teacherId, theMonth, year);
+  const availabilityHoursTutor = await publicService.timeAvailabilityPublic(teacherId, theMonth, year, page, limit);
 
   if (!availabilityHoursTutor) throw new ApiError(httpStatus.NOT_FOUND, 'Teacher don\'t have availability hours.');
 
-  res.sendWrapped(availabilityHoursTutor, httpStatus.OK);
+  res.sendWrapped('', httpStatus.OK, availabilityHoursTutor);
 });
 
 module.exports = {
