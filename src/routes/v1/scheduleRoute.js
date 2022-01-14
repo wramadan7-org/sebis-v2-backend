895f76faed420ddf282a0f98e68990540bfe385b
wramadan7-org <wramadan1203@gmail.com>
2022-01-14 15:15:20 +0700

    route schedule to create schedule

---
+++ src/routes/v1/scheduleRoute.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+
+const scheduleController = require('../../controllers/scheduleController');
+const validate = require('../../middlewares/validate');
+const auth = require('../../middlewares/auth');
+const scheduleValidation = require('../../validations/scheduleValidation');
+
+const router = Router();
+
+router.post('/', validate(scheduleValidation.createSchedule), scheduleController.createSchedule);
+
+module.exports = router;
