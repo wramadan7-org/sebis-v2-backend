f498fde8a3c506945df2a7b628e2c5d30d94840d
wramadan7-org <wramadan1203@gmail.com>
2022-01-18 13:42:28 +0700

    add migration to add teacherSubjectId in cartItems

---
+++ src/migrations/20220118063950-add-column-teacherSubjectId-in-cartItem.js
@@ -0,0 +1,26 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn(
+      'cartItems',
+      'teacherSubjectId',
+      {
+        type: Sequelize.STRING,
+        allowNull: true,
+        references: {
+          model: 'teacherSubjects',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        after: 'teacherId',
+      },
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn(
+      'cartItems',
+      'teacherSubjectId',
+    );
+  },
+};
