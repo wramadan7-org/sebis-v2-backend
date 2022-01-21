f3e6a8a6a4a021f0f65e11820cd41b7d0cef90f7
wramadan7-org <wramadan1203@gmail.com>
2022-01-21 13:26:09 +0700

    route to get own wishlist and wishlist by id

--- src/routes/v1/wishlistRoute.js
+++ src/routes/v1/wishlistRoute.js
@@ -5,5 +5,7 @@ const auth = require('../../middlewares/auth');
 const wishlistController = require('../../controllers/wishlistController');
 
 router.post('/', auth, wishlistController.addWishlist);
+router.get('/', auth, wishlistController.getWishlist);
+router.get('/:id', auth, wishlistController.getWihslistItemById);
 
 module.exports = router;
