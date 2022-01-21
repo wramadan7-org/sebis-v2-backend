5d64f2ffc35ce79dd58cd513823b32fb5d96eca3
wramadan7-org <wramadan1203@gmail.com>
2022-01-21 14:38:29 +0700

    simple result get wishlist

--- src/controllers/wishlistController.js
+++ src/controllers/wishlistController.js
@@ -9,6 +9,8 @@ const { Subject } = require('../models/Subject');
 const { Grade } = require('../models/Grade');
 const { AvailabilityHours } = require('../models/AvailabilityHours');
 const ApiError = require('../utils/ApiError');
+const days = require('../utils/day');
+const dates = require('../utils/date');
 
 const { OFFSET_ORDER_HOURS } = process.env;
 
@@ -121,7 +123,48 @@ const getWishlist = catchAsync(async (req, res) => {
     },
   );
 
-  res.sendWrapped(wishlist, httpStatus.OK);
+  // Ambil data original
+  const originalData = JSON.stringify(wishlist);
+  // Kemudian parsing ke JSON untuk pendefinisian
+  const convertData = JSON.parse(originalData);
+
+  const mapingData = convertData.map((o) => {
+    const arrayWishlistItem = [];
+
+    if (o.WishlistItems && o.WishlistItems.length > 0) {
+      o.WishlistItems.map((itm) => {
+        const convertDay = itm.availabilityHour ? days(itm.availabilityHour.dayCode) : days(moment(itm.dateTimeStart).day());
+        const convertDate = itm.dateTimeStart ? dates(itm.dateTimeStart) : null;
+
+        const dataWishlistItem = {
+          wishlistItemId: itm.id,
+          teacherSubjectId: itm.teacherSubjectId,
+          availabilityHoursId: itm.availabilityHoursId,
+          teacherId: itm.teacherId,
+          typeCourse: itm.typeCourse,
+          date: `${convertDay}, ${convertDate}`,
+          time: `${moment(itm.dateTimeStart).format('HH:mm')} - ${moment(itm.dateTimeEnd).format('HH:mm')}`,
+          subject: itm.teacherSubject.subject.subjectName,
+          grade: itm.teacherSubject.gradeName,
+        };
+
+        return arrayWishlistItem.push(dataWishlistItem);
+      });
+    }
+
+    const data = {
+      wishlistId: o.id,
+      studentId: o.studentId,
+      teacherId: o.teacherId,
+      student: `${o.student.firstName} ${o.student.lastName}`,
+      teacher: `${o.teacher.firstName} ${o.teacher.lastName}`,
+      wishlistItem: arrayWishlistItem,
+    };
+
+    return data;
+  });
+
+  res.sendWrapped(mapingData, httpStatus.OK);
 });
 
 const getWihslistItemById = catchAsync(async (req, res) => {
