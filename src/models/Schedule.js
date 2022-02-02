28066ad491335f1028572cf6a7b2384c744394b4
wramadan7-org <wramadan1203@gmail.com>
2022-02-02 22:29:28 +0700

    set createdAt and updatedAt second to 00 (default values)

--- src/models/Schedule.js
+++ src/models/Schedule.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const { sequelize, SequelizeInstance, DataTypes } = require('../config/database');
 
 const {
@@ -30,6 +31,16 @@ const Schedule = sequelize.define('schedule', {
   imageMaterial: {
     type: DataTypes.STRING,
     allowNull: true,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: moment().format('YYYY-MM-DD HH:mm:00'),
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: moment().format('YYYY-MM-DD HH:mm:00'),
   },
 }, {
   paranoid: true,
