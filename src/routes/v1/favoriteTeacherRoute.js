7a342f19f96c556f5b1f5f8631f86a4a8a01fedb
wramadan7-org <wramadan1203@gmail.com>
2022-02-12 12:47:39 +0700

    route update or create favorite teacher

--- src/routes/v1/favoriteTeacherRoute.js
+++ src/routes/v1/favoriteTeacherRoute.js
@@ -9,5 +9,6 @@ const favoriteTeacherController = require('../../controllers/favoriteTeacherCont
 router.post('/', auth, student, favoriteTeacherController.createFavorite);
 router.get('/', auth, favoriteTeacherController.getAllFavoriteTeacher);
 router.get('/own', auth, student, favoriteTeacherController.getMyFavoriteTeacher);
+router.patch('/', auth, favoriteTeacherController.updateOrCreateFavoriteTeacher);
 
 module.exports = router;
