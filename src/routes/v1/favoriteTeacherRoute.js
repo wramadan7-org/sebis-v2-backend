97f2d29406986848240198e040ad234775061ab4
wramadan7-org <wramadan1203@gmail.com>
2022-02-12 11:48:51 +0700

    route get my favorite teacher

--- src/routes/v1/favoriteTeacherRoute.js
+++ src/routes/v1/favoriteTeacherRoute.js
@@ -7,5 +7,6 @@ const { student } = require('../../middlewares/roleValidation');
 const favoriteTeacherController = require('../../controllers/favoriteTeacherController');
 
 router.post('/', auth, student, favoriteTeacherController.createFavorite);
+router.get('/', auth, student, favoriteTeacherController.getMyFavoriteTeacher);
 
 module.exports = router;
